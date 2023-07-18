e9e324c7b94141ffb1cea9d0c48d102129b21f2b
Bianca Ailincai <ailincaibianca09@gmail.com>
2023-07-18 14:05:58 +0300

    FIG-31380: 'onGoBack' support for 'OverlayHeader'

--- packages/ui/overlay/header/overlayHeader.jsx
+++ packages/ui/overlay/header/overlayHeader.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 
 import { IconButton } from "../../button";
+import ArrowLeftMedium from "../../icons/arrow/left/medium";
 import Cancel from "../../icons/cancel/large";
 import { OverlayContext } from "../overlay";
 
@@ -15,31 +16,37 @@ export default class OverlayHeader extends Component {
   static contextType = OverlayContext;
   static propTypes = {
     /**
-      Optional class name to append to the wrapping node.
+      Optional overlay back button, displayed next to the title.
+      Triggered when the "Go back" header button is clicked.
+     */
+    backBtnFn: PropTypes.func,
+    /**
+     Optional class name to append to the wrapping node.
      */
     className: PropTypes.string,
     /**
-      Optional overlay description, to be displayed under the title.
+     Optional overlay description, to be displayed under the title.
      */
     description: PropTypes.node,
     /**
-      Set this to `true` if you render `OverlayHeader` inside an `OverlayContent` component.
-      It will remove top and side paddings, and appropriately fit as a child of that component.
-      Useful in some cases where we want the OverlayHeader to scroll with the `Content`.
+     Set this to `true` if you render `OverlayHeader` inside an `OverlayContent` component.
+     It will remove top and side paddings, and appropriately fit as a child of that component.
+     Useful in some cases where we want the OverlayHeader to scroll with the `Content`.
      */
     isPartOfContent: PropTypes.bool,
     /**
-      Text or content for the Overlay title.
+     Text or content for the Overlay title.
      */
     title: PropTypes.node,
     /**
      Triggered when the "Close" header button is clicked.
      If not provided, will be inherited from the `Overlay` component context.
-    */
+     */
     onClose: PropTypes.func,
   }
 
   static defaultProps = {
+    backBtnFn: undefined,
     className: undefined,
     description: undefined,
     isPartOfContent: false,
@@ -48,7 +55,7 @@ export default class OverlayHeader extends Component {
   }
 
   render() {
-    const { className, isPartOfContent, description, title, onClose, ...props } = this.props;
+    const { backBtnFn, className, isPartOfContent, description, title, onClose, ...props } = this.props;
     const overlayId = this.context?.id ?? 0;
     const isForConfirmationOverlay = CONFIRMATION_OVERLAYS.includes(this.context?.background);
     const ariaTitle = `dialog-${overlayId}-title`;
@@ -62,16 +69,29 @@ export default class OverlayHeader extends Component {
           [styles.confirmation]: isForConfirmationOverlay,
         }, className)}
       >
-        <div>
-          <h1 className={styles.title} id={ariaTitle}>{title}</h1>
-          {description && <p className={styles.description} id={ariaDescription}>{description}</p>}
+        <div className={styles.headerInfo}>
+          {backBtnFn && (
+            <IconButton
+              Icon={ArrowLeftMedium}
+              className={styles.iconBtn}
+              size="M"
+              tabIndex={-1}
+              theme="tertiary"
+              onClick={backBtnFn}
+            >
+              Go back
+            </IconButton>)}
+          <div>
+            <h1 className={styles.title} id={ariaTitle}>{title}</h1>
+            {description && <p className={styles.description} id={ariaDescription}>{description}</p>}
+          </div>
         </div>
         <IconButton
           Icon={Cancel}
-          className={styles.closeButton}
+          className={styles.iconBtn}
           size="M"
           tabIndex={-1}
-          theme="primary"
+          theme="tertiary"
           onClick={onClose ?? this.context.onClose}
         >
           Close
