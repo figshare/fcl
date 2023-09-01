a428085f1422312998e0a6a4bf7bdea96db1c667
Razvan Antalut <razvan@figshare.com>
2023-09-01 14:47:27 +0300

    FIG-32303: added Secondary and Primary OverlayFooter slot components

--- packages/ui/overlay/footer/overlayFooter.jsx
+++ packages/ui/overlay/footer/overlayFooter.jsx
@@ -1,20 +1,20 @@
 import classnames from "classnames";
-import PropTypes from "prop-types";
+import { node, string } from "prop-types";
 import React, { Component } from "react";
 
 import style from "./overlayFooter.css";
 
 
-export default class OverlayFooter extends Component {
+export class OverlayFooter extends Component {
   static propTypes = {
     /**
       Contents for the overlay footer, usually a set of action buttons to close or save the overlay.
      */
-    children: PropTypes.node,
+    children: node,
     /**
       Optional class to append to the footer node.
      */
-    className: PropTypes.string,
+    className: string,
   }
 
   static defaultProps = {
@@ -35,3 +35,48 @@ export default class OverlayFooter extends Component {
     );
   }
 }
+
+export function SecondaryActions({ children, className, ...props }) {
+  return <div className={classnames(style.secondary, className)} {...props}>{children}</div>;
+}
+
+SecondaryActions.propTypes = {
+  /**
+  Contents for the overlay footer slot.
+  */
+  children: node,
+  /**
+    Optional class to append to the footer slot node.
+    */
+  className: string,
+};
+
+SecondaryActions.defaultProps = {
+  children: undefined,
+  className: undefined,
+};
+
+export function PrimaryActions({ children, className, ...props }) {
+  return <div className={classnames(style.primary, className)} {...props}>{children}</div>;
+}
+
+PrimaryActions.propTypes = {
+  /**
+  Contents for the overlay footer slot.
+  */
+  children: node,
+  /**
+    Optional class to append to the footer slot node.
+    */
+  className: string,
+};
+
+PrimaryActions.defaultProps = {
+  children: undefined,
+  className: undefined,
+};
+
+OverlayFooter.Secondary = SecondaryActions;
+OverlayFooter.Primary = PrimaryActions;
+
+export default OverlayFooter;
--- packages/ui/overlay/footer/overlayFooter.test.jsx
+++ packages/ui/overlay/footer/overlayFooter.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 import OverlayFooter from "./overlayFooter";
 
@@ -34,5 +34,24 @@ describe("<OverlayFooter />", () => {
 
     expect(component.isEmptyRender()).toEqual(false);
     expect(component.find(".test")).toHaveLength(1);
+  });
+
+  it("exposes slot components for primary and secondary actions", () => {
+    const component = mount(
+      <OverlayFooter className="test">
+        <OverlayFooter.Primary className="custom-main-slot">
+          Main Footer Actions
+        </OverlayFooter.Primary>
+        <OverlayFooter.Secondary className="custom-additional-slot">
+          Additional Footer Actions
+        </OverlayFooter.Secondary>
+      </OverlayFooter>);
+
+    expect(component.find(".primary.custom-main-slot")).toHaveLength(1);
+    expect(component.find(".secondary.custom-additional-slot")).toHaveLength(1);
+    expect(component.find(".primary.custom-main-slot").text()).toEqual("Main Footer Actions");
+    expect(component.find(".secondary.custom-additional-slot").text()).toEqual("Additional Footer Actions");
+
+    component.unmount();
   });
 });
