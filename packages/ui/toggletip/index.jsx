9264c032982badc9517232b1fcb25dd7afaa6d93
Bianca Ailincai <ailincaibianca09@gmail.com>
2024-02-08 12:56:04 +0200

    FIG-34935: Create new item - Multiple item type tooltips- fix

--- packages/ui/toggletip/index.jsx
+++ packages/ui/toggletip/index.jsx
@@ -5,6 +5,7 @@ import { Manager as PopperManager } from "react-popper";
 import { LinkingProvider } from "../a11y/linking";
 import { Provider } from "../popup/context";
 import uncontrollable from "../helpers/uncontrollable";
+import uuid from "../helpers/utils/uuid";
 
 
 export { Content, Trigger } from "../popup";
@@ -13,11 +14,12 @@ export { Content, Trigger } from "../popup";
 export class Toggletip extends Component {
   static propTypes = {
     children: PropTypes.func.isRequired,
+    closeSelf: PropTypes.bool,
     isVisible: PropTypes.bool,
     onToggle: PropTypes.func,
   }
 
-  static defaultProps = { onToggle: () => undefined, isVisible: undefined }
+  static defaultProps = { onToggle: () => undefined, isVisible: undefined, closeSelf: false }
 
   constructor(...args) {
     super(...args);
@@ -41,6 +43,27 @@ export class Toggletip extends Component {
     return { context: { ...prevState.context, isVisible } };
   }
 
+  toggletipId = uuid();
+
+  componentDidMount() {
+    if (this.props.closeSelf) {
+      document.body.addEventListener("toggletip-opened", this.listener);
+    }
+  }
+
+  componentDidUpdate(prepProps, prevState) {
+    if (prevState.context.isVisible !== this.state.context.isVisible && this.state.context.isVisible === true) {
+      const ev = new CustomEvent("toggletip-opened", { detail: { id: this.toggletipId } });
+      document.body.dispatchEvent(ev);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.closeSelf) {
+      document.body.removeEventListener("toggletip-opened", this.listener);
+    }
+  }
+
   render() {
     return (
       <Provider value={this.state.context}>
@@ -62,13 +85,22 @@ export class Toggletip extends Component {
   });
 
   onToggle = (event) => {
-    event?.stopPropagation();
+    event?.stopPropagation?.();
     this.props.onToggle(event, { isVisible: !this.state.context.isVisible });
   }
 
   onClose = (e) => {
     this.onToggle(e);
   }
+
+  listener = (event) => {
+    const { id } = event.detail;
+
+    if (id !== this.toggletipId) {
+      this.props.onToggle(event, { isVisible: false });
+    }
+  };
+
 }
 
 export const UncontrolledToggletip = uncontrollable(
