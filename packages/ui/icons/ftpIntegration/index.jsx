10d7a8f9195feca332a7af73df71b3c278a8a5f9
Alina <alina@figshare.com>
2024-11-18 10:03:25 +0200

    Rename ftp integration icon

--- packages/ui/icons/ftpIntegration/index.jsx
+++ packages/ui/icons/ftpIntegration/index.jsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from "react";
 
 
-export default class FTP extends PureComponent {
+export default class FTPIntegration extends PureComponent {
   render() {
     return (
       <svg
